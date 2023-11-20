164465d98a41fb4ce78423c22aa88cbbef78ecc6
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2023-11-20 19:40:30 +0530

    Add files via upload

---
+++ CalorieTrackerApp/DAL/Repository/CalorieTrackerRepository.cs
@@ -0,0 +1,64 @@
+﻿using CalorieTrackerApp.DAL.Interface;
+using CalorieTrackerApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace CalorieTrackerApp.DAL.Repository
+{
+    public class CalorieTrackerRepository : ICalorieTrackerRepository
+    {
+        private CalorieTrackerDbContext _context;
+        public CalorieTrackerRepository(CalorieTrackerDbContext Context)
+        {
+            this._context = Context;
+        }
+        public IEnumerable<Calorie> GetCalories()
+        {
+             return _context.Calories.ToList();
+        }
+        public Calorie GetCalorieByID(int id)
+        {
+            return _context.Calories.Find(id);
+        }
+        public Calorie InsertCalorie(Calorie Calorie)
+        {
+            return _context.Calories.Add(Calorie);
+        }
+        public int DeleteCalorie(int CalorieID)
+        {
+            Calorie Calorie = _context.Calories.Find(CalorieID);
+            var res= _context.Calories.Remove(Calorie);
+            return res.Id;
+        }
+        public bool UpdateCalorie(Calorie Calorie)
+        {
+            var res= _context.Entry(Calorie).State = EntityState.Modified;
+            return res.Equals("Calorie");
+        }
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+        private bool disposed = false;
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ CalorieTrackerApp/Migrations/202311031816176_Initial.cs
@@ -0,0 +1,16 @@
+namespace CalorieTrackerApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
