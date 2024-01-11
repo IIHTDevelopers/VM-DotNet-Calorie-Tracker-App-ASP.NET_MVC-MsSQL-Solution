a2b131cf1d3451eae96186c7ed15779f64b6463b
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-01-11 10:55:58 +0530

    Add files via upload

---
+++ CalorieTrackerApp/DAL/Repository/CalorieTrackerService.cs
@@ -0,0 +1,47 @@
+﻿using CalorieTrackerApp.DAL.Interface;
+using CalorieTrackerApp.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CalorieTrackerApp.DAL.Repository
+{
+    public class CalorieTrackerService : ICalorieTrackerInterface
+    {
+        private ICalorieTrackerRepository _repo;
+        public CalorieTrackerService(ICalorieTrackerRepository repo)
+        {
+            this._repo = repo;
+        }
+
+        public int DeleteCalorie(int CalorieId)
+        {
+            var res= _repo.DeleteCalorie(CalorieId);
+            return res;
+        }
+
+        public Calorie GetCalorieByID(int CalorieId)
+        {
+            return _repo.GetCalorieByID(CalorieId);
+        }
+        public void Save()
+        {
+            _repo.Save();
+        }
+
+
+        IEnumerable<Calorie> ICalorieTrackerInterface.GetCalories()
+        {
+            return _repo.GetCalories();
+        }
+
+        Calorie ICalorieTrackerInterface.InsertCalorie(Calorie Calorie)
+        {
+            return _repo.InsertCalorie(Calorie);
+        }
+
+        bool ICalorieTrackerInterface.UpdateCalorie(Calorie Calorie)
+        {
+            return _repo.UpdateCalorie(Calorie);
+        }
+    }
+}
